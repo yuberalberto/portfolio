7899847a25b1234c1d83d1c666070ca8c1d8991a
yuberalberto <yuberalberto@gmail.com>
2019-02-08 19:09:59 -0500

    page working, avatar not working

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content  } from 'react-mdl';
-import Main from './components/Main'
+import Main from './components/Main';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
+  render(){
+    return(
       <div className="demo-big-content">
-          <Layout>
-              <Header title="Title" scroll>
-                  <Navigation>
-                      <a href="/">Link</a>
-                      <a href="/">Link</a>
-                      <a href="/">Link</a>
-                      <a href="/">Link</a>
-                  </Navigation>
-              </Header>
-              <Drawer title="Title">
-                  <Navigation>
-                      <a href="/">Link</a>
-                      <a href="/">Link</a>
-                      <a href="/">Link</a>
-                      <a href="/">Link</a>
-                  </Navigation>
-              </Drawer>
-              <Content>
-                  <div className="page-content" />
-                  <Main/>
-              </Content>
-          </Layout>
+        <Layout>
+          <Header className='header-color' title='My Portfolio' scroll>
+            <Navigation>
+              <Link to="/Resume">Resume</Link>
+              <Link to="/About">About Me</Link>
+              <Link to="/Projects">Projects</Link>
+              <Link to="/Contact">Contact</Link>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <Link to="/">Main Page</Link>
+              <Link to="/Resume">Resume</Link>
+              <Link to="/About">About Me</Link>
+              <Link to="/Projects">Projects</Link>
+              <Link to="/Contact">Contact</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main/>
+          </Content>
+        </Layout>
       </div>
     );
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import Landing from './Landingpage';
 import { Switch, Route} from 'react-router-dom'
+import Landing from './Landingpage';
+import About from './About';
+import Contact from './Contact';
+import Projects from './Projects';
+import Resume from './Resume';
+
 
 const Main = () => (
   <Switch>
     <Route exact path='/' component={Landing}/>
+    <Route path='/About' component={About}/>
+    <Route path='/Contact' component={Contact}/>
+    <Route path='/Projects' component={Projects}/>
+    <Route path='/Resume' component={Resume}/>
+
   </Switch>
 )
 
